c7e8ace8741c46473a02afb6b3c223c12b3afbd5
Michele Cianni <cianni.michele.private@gmail.com>
2026-06-01 14:31:28 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::os::unix::fs::PermissionsExt;
+use std::path::{Path, PathBuf};
 
 fn print_prompt() -> io::Result<()> {
     print!("$ ");
@@ -16,6 +19,13 @@ fn is_builtin(command: &str) -> bool {
     matches!(command, "exit" | "echo" | "type")
 }
 
+fn get_command_in_path(command_name: &str, path: &PathBuf) -> PathBuf {
+    let mut command_path = path.clone();
+    command_path.push(command_name);
+    command_path
+}
+
+
 fn handle_type_command(args: &[&str]) {
     if args.is_empty() {
         println!("type: missing operand");
@@ -26,9 +36,23 @@ fn handle_type_command(args: &[&str]) {
 
     if is_builtin(target) {
         println!("{target} is a shell builtin");
+        return;
     } else {
-        println!("{target}: not found");
+        let paths = paths_from_env();
+
+        for p in paths {
+            let buffer = &get_command_in_path(target, &p);
+            let path = Path::new(buffer);
+            if path.exists() && path.is_file() {
+                if (path.metadata().unwrap().permissions().mode() & 0o111) != 0 {
+                    println!("{} is {}", target, path.to_str().unwrap().to_string());
+                    return;
+                }
+            }
+        }
     }
+
+    println!("{target}: not found");
 }
 
 fn handle_echo_command(args: &[&str]) {
@@ -42,25 +66,38 @@ fn handle_command(line: &str) -> bool {
     }
 
     let mut parts = line.split_whitespace();
-    let Some(cmd) = parts.next() else { return true; };
-    let args:Vec<&str> = parts.collect();
+    let Some(cmd) = parts.next() else {
+        return true;
+    };
+    let args: Vec<&str> = parts.collect();
 
     match cmd {
         "type" => {
             handle_type_command(&args);
-            return true;
-        },
+            true
+        }
         "echo" => {
             handle_echo_command(&args);
-            return true;
+            true
         }
         "exit" => {
-            return false; // exit the shell
+            false // exit the shell
         }
         _ => {
             println!("{cmd}: command not found");
             true
         }
+    }
+}
+
+fn paths_from_env() -> Vec<PathBuf> {
+    match env::var_os("PATH") {
+
+        Some(paths) => {
+            env::split_paths(&paths).collect()
+        }
+
+        None => Vec::new(),
     }
 }
 
