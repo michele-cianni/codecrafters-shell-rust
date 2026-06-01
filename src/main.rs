3cfca0ead7e5b11c99276be5e1fd1ffa3b6ad98b
Michele Cianni <cianni.michele.private@gmail.com>
2026-06-01 11:21:14 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn print_prompt() -> io::Result<()> {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()
+}
 
-    // Wait for user input
+fn read_command_line() -> io::Result<String> {
     let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim())
+    io::stdin().read_line(&mut command)?;
+    Ok(command.trim().to_string())
+}
+
+fn print_command_not_found(command: &str) {
+    println!("{command}: command not found");
+}
+
+fn main() -> io::Result<()> {
+    print_prompt()?;
+    let command = read_command_line()?;
+    print_command_not_found(&command);
+    Ok(())
 }
