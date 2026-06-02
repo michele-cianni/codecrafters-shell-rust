0bb720871032221c12afcf8177b5035992b5cba7
Michele Cianni <cianni.michele.private@gmail.com>
2026-06-03 00:18:04 +0200

    codecrafters submit [skip ci]

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -1,9 +1,11 @@
-use std::env;
-use std::path::Path;
 use crate::builtins::CommandHandler;
 use crate::shell::command::CommandResult;
+use std::env;
+use std::path::PathBuf;
 
 pub struct CdHandler;
+
+const HOME: &'static str = "HOME";
 
 impl CommandHandler for CdHandler {
     fn execute(&self, args: &[&str]) -> CommandResult {
@@ -12,7 +14,25 @@ impl CommandHandler for CdHandler {
             return CommandResult::Continue;
         };
 
-        let path = Path::new(target);
+        let path = if target == "~" {
+            match env::var_os(HOME) {
+                Some(home) => PathBuf::from(home),
+                None => {
+                    println!("cd: could not determine home directory");
+                    return CommandResult::Continue;
+                }
+            }
+        } else if let Some(stripped) = target.strip_prefix("~/") {
+            match env::var_os(HOME) {
+                Some(home) => PathBuf::from(home).join(stripped),
+                None => {
+                    println!("cd: could not determine home directory");
+                    return CommandResult::Continue;
+                }
+            }
+        } else {
+            PathBuf::from(target)
+        };
 
         if !path.exists() || !path.is_dir() {
             println!("cd: {}: No such file or directory", path.display());
