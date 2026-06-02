d9d0ad171d6c46d32c7614abdf143e81b4dd62a8
Michele Cianni <cianni.michele.private@gmail.com>
2026-06-02 23:55:45 +0200

    codecrafters submit [skip ci]

--- src/builtins/pwd.rs
+++ src/builtins/pwd.rs
@@ -5,7 +5,12 @@ use crate::shell::command::CommandResult;
 pub struct PwdHandler;
 
 impl CommandHandler for PwdHandler {
-    fn execute(&self, _args: &[&str]) -> CommandResult {
+    fn execute(&self, args: &[&str]) -> CommandResult {
+        if !args.is_empty() {
+            println!("pwd: arguments not supported");
+            return CommandResult::Continue;
+        }
+
         match env::current_dir() {
             Ok(dir) => println!("{}", dir.display()),
             Err(err) => println!("pwd: {err}"),
